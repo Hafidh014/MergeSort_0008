98764b0f017ee74a56a0a4869627a01bf2d1531e
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-02 15:07:10 +0700

    menghubungkan 2 bagian

--- MergeShort.cpp
+++ MergeShort.cpp
@@ -39,6 +39,13 @@ void mergeSort(int low, int high)
     if (low >= high)
     {
         return;
-    } 
+    }
+
+    int mid = (low + high) / 2;
+
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high); 
+
 }
 
